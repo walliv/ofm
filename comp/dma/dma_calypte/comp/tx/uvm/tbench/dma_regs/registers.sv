b85c2cec83ca49c66f16964169efdd27febc3a30
Vladislav Valek <vladislav.valek@email.cz>
2024-04-18 08:59:27 +0200

    registers.sv [MAINTENANCE]: remove unnecessary addr_register class
    The addr_register is not needed since addresses of data buffers are
    handed over a Device Tree.

--- comp/dma/dma_calypte/comp/tx/uvm/tbench/dma_regs/registers.sv
+++ comp/dma/dma_calypte/comp/tx/uvm/tbench/dma_regs/registers.sv
@@ -87,35 +87,6 @@ class pointer_register extends uvm_reg;
     endfunction
 endclass
 
-
-class addr_register extends uvm_reg;
-    `uvm_object_utils(uvm_dma_regs::addr_register)
-
-    // Write
-    rand uvm_reg_field addr;
-
-    function new(string name = "addr_register");
-        super.new(name, 64, UVM_NO_COVERAGE);
-    endfunction
-
-    virtual function void build();
-        //Create fields
-        addr = uvm_reg_field::type_id::create("addr");
-        //Configure
-        addr.configure(this, // Parent
-                       64   , // Number of bits
-                       0  , // LSB
-                       "RW", // Access
-                       0   , // Volatility
-                       0   , // Value on reset
-                       1   , // Can the value be reset?
-                       1   , // Can the value be randomized?
-                       0     // Does the field occupy an entire byte lane?
-                       );
-    endfunction
-endclass
-
-
 class cnt_register extends uvm_reg;
     `uvm_object_utils(uvm_dma_regs::cnt_register)
 
