a07b08984b2a41b90fd4a468e7068b8d2e2e052e
Vladislav Valek <vladislav.valek@email.cz>
2024-07-04 10:08:05 +0200

    dut.sv [MAINTENANCE]: remove "tx" from USR_* interfaces and signals
    It does not add any informational value.

--- comp/dma/dma_calypte/comp/tx/uvm/tbench/dut.sv
+++ comp/dma/dma_calypte/comp/tx/uvm/tbench/dut.sv
@@ -11,12 +11,12 @@ module DUT (
     input logic     CLK,
     input logic     RST,
     mfb_if.dut_rx   cq_mfb,
-    mfb_if.dut_tx   usr_tx_mfb,
+    mfb_if.dut_tx   usr_mfb,
     mi_if.dut_slave config_mi
 );
 
-    localparam USR_SOF_POS_WIDTH = (($clog2(USR_TX_MFB_REGION_SIZE)*USR_TX_MFB_REGIONS) == 0) ? (USR_TX_MFB_REGIONS) : (USR_TX_MFB_REGIONS*$clog2(USR_TX_MFB_REGION_SIZE));
-    localparam USR_EOF_POS_WIDTH = USR_TX_MFB_REGIONS*$clog2(USR_TX_MFB_REGION_SIZE*USR_TX_MFB_BLOCK_SIZE);
+    localparam USR_SOF_POS_WIDTH = (($clog2(USR_MFB_REGION_SIZE)*USR_MFB_REGIONS) == 0) ? (USR_MFB_REGIONS) : (USR_MFB_REGIONS*$clog2(USR_MFB_REGION_SIZE));
+    localparam USR_EOF_POS_WIDTH = USR_MFB_REGIONS*$clog2(USR_MFB_REGION_SIZE*USR_MFB_BLOCK_SIZE);
 
     logic [$clog2(PKT_SIZE_MAX+1)-1:0] packet_size;
     logic [$clog2(CHANNELS)-1:0]       channel;
@@ -25,9 +25,9 @@ module DUT (
 
     generate
         //{packet_size, channel, meta} 24 + $clog2(PKT_SIZE_MAX+1) + $clog2(CHANNELS)[CHANNELS]
-        assign usr_tx_mfb.META[24 + $clog2(PKT_SIZE_MAX+1) + $clog2(CHANNELS)-1 -: $clog2(PKT_SIZE_MAX+1)] = packet_size[$clog2(PKT_SIZE_MAX+1)-1 -: $clog2(PKT_SIZE_MAX+1)];
-        assign usr_tx_mfb.META[24 + $clog2(CHANNELS)-1                          -: $clog2(CHANNELS)]       = channel[$clog2(CHANNELS)-1 -: $clog2(CHANNELS)]                ;
-        assign usr_tx_mfb.META[24 - 1                                           -: 24]                     = meta[24-1 -: 24]                                               ;
+        assign usr_mfb.META[24 + $clog2(PKT_SIZE_MAX+1) + $clog2(CHANNELS)-1 -: $clog2(PKT_SIZE_MAX+1)] = packet_size[$clog2(PKT_SIZE_MAX+1)-1 -: $clog2(PKT_SIZE_MAX+1)];
+        assign usr_mfb.META[24 + $clog2(CHANNELS)-1                          -: $clog2(CHANNELS)]       = channel[$clog2(CHANNELS)-1 -: $clog2(CHANNELS)]                ;
+        assign usr_mfb.META[24 - 1                                           -: 24]                     = meta[24-1 -: 24]                                               ;
     endgenerate
 
     assign cq_mfb_sof_int = '0;
@@ -37,10 +37,10 @@ module DUT (
 
         .MI_WIDTH                 (MI_WIDTH),
 
-        .USR_TX_MFB_REGIONS       (USR_TX_MFB_REGIONS),
-        .USR_TX_MFB_REGION_SIZE   (USR_TX_MFB_REGION_SIZE),
-        .USR_TX_MFB_BLOCK_SIZE    (USR_TX_MFB_BLOCK_SIZE),
-        .USR_TX_MFB_ITEM_WIDTH    (USR_TX_MFB_ITEM_WIDTH),
+        .USR_TX_MFB_REGIONS       (USR_MFB_REGIONS),
+        .USR_TX_MFB_REGION_SIZE   (USR_MFB_REGION_SIZE),
+        .USR_TX_MFB_BLOCK_SIZE    (USR_MFB_BLOCK_SIZE),
+        .USR_TX_MFB_ITEM_WIDTH    (USR_MFB_ITEM_WIDTH),
 
         .PCIE_CQ_MFB_REGIONS      (PCIE_CQ_MFB_REGIONS),
         .PCIE_CQ_MFB_REGION_SIZE  (PCIE_CQ_MFB_REGION_SIZE),
@@ -75,13 +75,13 @@ module DUT (
         .USR_TX_MFB_META_CHAN     (channel),
         .USR_TX_MFB_META_HDR_META (meta),
 
-        .USR_TX_MFB_DATA          (usr_tx_mfb.DATA),
-        .USR_TX_MFB_SOF           (usr_tx_mfb.SOF),
-        .USR_TX_MFB_EOF           (usr_tx_mfb.EOF),
-        .USR_TX_MFB_SOF_POS       (usr_tx_mfb.SOF_POS),
-        .USR_TX_MFB_EOF_POS       (usr_tx_mfb.EOF_POS),
-        .USR_TX_MFB_SRC_RDY       (usr_tx_mfb.SRC_RDY),
-        .USR_TX_MFB_DST_RDY       (usr_tx_mfb.DST_RDY),
+        .USR_TX_MFB_DATA          (usr_mfb.DATA),
+        .USR_TX_MFB_SOF           (usr_mfb.SOF),
+        .USR_TX_MFB_EOF           (usr_mfb.EOF),
+        .USR_TX_MFB_SOF_POS       (usr_mfb.SOF_POS),
+        .USR_TX_MFB_EOF_POS       (usr_mfb.EOF_POS),
+        .USR_TX_MFB_SRC_RDY       (usr_mfb.SRC_RDY),
+        .USR_TX_MFB_DST_RDY       (usr_mfb.DST_RDY),
 
         .PCIE_CQ_MFB_DATA         (cq_mfb.DATA),
         .PCIE_CQ_MFB_META         (cq_mfb.META),
