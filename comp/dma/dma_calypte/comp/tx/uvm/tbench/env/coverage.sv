7d098dc11752e88ef9945ed2b99bb36688e85ea6
Vladislav Valek <vladislav.valek@email.cz>
2024-07-04 10:10:34 +0200

    coverage.sv [STYLE]: align class header declaration

--- comp/dma/dma_calypte/comp/tx/uvm/tbench/env/coverage.sv
+++ comp/dma/dma_calypte/comp/tx/uvm/tbench/env/coverage.sv
@@ -4,7 +4,9 @@
 
 // SPDX-License-Identifier: BSD-3-Clause
 
-class coverage #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) extends uvm_subscriber#(uvm_mfb::sequence_item #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH));
+class coverage #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH)
+    extends uvm_subscriber #(uvm_mfb::sequence_item #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH));
+
     `uvm_component_param_utils(uvm_tx_dma_calypte::coverage #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH))
 
     covergroup m_cov_seq_item_sof_eof_count with function sample(uvm_mfb::sequence_item #(MFB_REGIONS, MFB_REGION_SIZE, MFB_BLOCK_SIZE, MFB_ITEM_WIDTH, MFB_META_WIDTH) seq_item);
