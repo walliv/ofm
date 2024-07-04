6d5af2811a86ffc8cc9b9675d9d48d4ba96acf63
Vladislav Valek <vladislav.valek@email.cz>
2024-07-04 11:02:32 +0200

    tests/pkg [MAINTENANCE]: remove TX part of the MFB parameters name

--- comp/dma/dma_calypte/comp/tx/uvm/tbench/tests/base.sv
+++ comp/dma/dma_calypte/comp/tx/uvm/tbench/tests/base.sv
@@ -12,10 +12,10 @@ class base extends uvm_test;
         DEVICE,
         MI_WIDTH,
 
-        USR_TX_MFB_REGIONS,
-        USR_TX_MFB_REGION_SIZE,
-        USR_TX_MFB_BLOCK_SIZE,
-        USR_TX_MFB_ITEM_WIDTH,
+        USR_MFB_REGIONS,
+        USR_MFB_REGION_SIZE,
+        USR_MFB_BLOCK_SIZE,
+        USR_MFB_ITEM_WIDTH,
 
         PCIE_CQ_MFB_REGIONS,
         PCIE_CQ_MFB_REGION_SIZE,
@@ -49,21 +49,21 @@ class base extends uvm_test;
     endfunction
 
     function void build_phase(uvm_phase phase);
-                                  USR_TX_MFB_REGIONS, USR_TX_MFB_REGION_SIZE, USR_TX_MFB_BLOCK_SIZE, USR_TX_MFB_ITEM_WIDTH,
                                   PCIE_CQ_MFB_REGIONS, PCIE_CQ_MFB_REGION_SIZE, PCIE_CQ_MFB_BLOCK_SIZE, PCIE_CQ_MFB_ITEM_WIDTH,
                                   CHANNELS, DATA_POINTER_WIDTH, PKT_SIZE_MAX, PCIE_LEN_MAX )::type_id::create("m_env", this);
         m_env = uvm_tx_dma_calypte::env #(DEVICE, MI_WIDTH,
+                                          USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE, USR_MFB_ITEM_WIDTH,
     endfunction
 
     // ------------------------------------------------------------------------
     // Create environment and Run sequences o their sequencers
     virtual task run_phase(uvm_phase phase);
         time time_start;
-        virt_seq #(USR_TX_MFB_REGIONS, USR_TX_MFB_REGION_SIZE, USR_TX_MFB_BLOCK_SIZE, USR_TX_MFB_ITEM_WIDTH,
+        virt_seq #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE, USR_MFB_ITEM_WIDTH,
                    CHANNELS, HDR_META_WIDTH, PKT_SIZE_MAX) m_virt_seq;
 
         //CREATE SEQUENCES
-        m_virt_seq = virt_seq #(USR_TX_MFB_REGIONS, USR_TX_MFB_REGION_SIZE, USR_TX_MFB_BLOCK_SIZE, USR_TX_MFB_ITEM_WIDTH,
+        m_virt_seq = virt_seq #(USR_MFB_REGIONS, USR_MFB_REGION_SIZE, USR_MFB_BLOCK_SIZE, USR_MFB_ITEM_WIDTH,
                                 CHANNELS, HDR_META_WIDTH, PKT_SIZE_MAX)::type_id::create("m_virt_seq");
 
         //RISE OBJECTION
