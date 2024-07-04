b25ee77bc55766116f5517cc7a4026001759470d
Vladislav Valek <vladislav.valek@email.cz>
2024-07-04 10:40:29 +0200

    env.sv [STYLE]: adjust header

--- comp/dma/dma_calypte/comp/tx/uvm/tbench/rx_env/env.sv
+++ comp/dma/dma_calypte/comp/tx/uvm/tbench/rx_env/env.sv
@@ -1,8 +1,9 @@
-//-- env.sv
-//-- Copyright (C) 2022 CESNET z. s. p. o.
-//-- Author(s): Daniel Kriz <danielkriz@cesnet.cz>
+// env.sv
+// Copyright (C) 2022-2024 CESNET z. s. p. o.
+// Author(s): Daniel Kriz <danielkriz@cesnet.cz>
+//            Vladislav Valek <valekv@cesnet.cz>
 
-//-- SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: BSD-3-Clause
 
 class env #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, CHANNELS, PCIE_MTU, DATA_ADDR_W, DEVICE) extends uvm_env;
     `uvm_component_param_utils(uvm_dma_ll_rx::env #(REGIONS, REGION_SIZE, BLOCK_SIZE, ITEM_WIDTH, CHANNELS, PCIE_MTU, DATA_ADDR_W, DEVICE));
